83c5236a7d1c21e9d581aa4c385888a396b86623
Daniel Cardoso <daniel.dev.cardoso@gmail.com>
2025-03-28 10:22:33 -0300

    ajuste foto

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -14,22 +14,22 @@ export default function Hero() {
 
   return (
     <section id="home" className="bg-gray-950 text-white pt-20 mb-6 lg:mb-0">
-      <div className="mx-2 lg:mx-0 md:p-4  flex justify-center items-start ">
-        <div className=" md:w-[765px] py-2 flex flex-col items-center md:items-center lg:items-start  space-y-4 text-center lg:py-24    ">
+      <div className="mx-2 lg:mx-0 md:p-4  flex justify-center  items-start ">
+        <div className="  md:w-[765px] py-2 flex flex-col items-center md:items-center lg:items-start   space-y-4 text-center lg:py-24    ">
           <h1 className="text-2xl font-semibold lg:py-2 font-oxanium text-center lg:text-start tracking-tighter  sm:text-5xl md:text-6xl">
             Transformando <span className="text-emerald-500">pessoas</span> e processos em{" "}
             <span className="text-emerald-500">
               resultados extraordinários!{" "}
             </span>
           </h1>
-          <p className=" min-w-[200px] max-w-[500] md:w-[750px] md:max-w[1000] text-base md:text-lg lg:text-xl text-center lg:text-start text-gray-100 font-oxanium sm:text-xl  ">
+          <p className=" min-w-[200px] max-w-[500] md:w-[750px] md:max-w[1000]  text-base md:text-lg lg:text-xl text-center lg:text-start text-gray-100 font-oxanium sm:text-xl  ">
             <span className=" shadow-sm">Consultoria</span> e
             <span className="  shadow-sm">coaching </span>especializados para
             elevar o desempenho da sua equipe e impulsionar o sucesso do
             negócio.
           </p>
 
-          <p className="min-w-[200px] max-w-[500] md:w-[750px] md:max-w[1000] text-base md:text-lg lg:text-xl text-center lg:text-start text-gray-100 font-oxanium sm:text-xl  ">
+          <p className="min-w-[200px] max-w-[500] md:w-[750px] md:max-w[1000] text-base md:text-lg lg:text-xl  text-center lg:text-start text-gray-100 font-oxanium sm:text-xl  ">
             Temos{" "}
             <span className="text-emerald-500 shadow-sm">
               {" "}
@@ -78,8 +78,8 @@ export default function Hero() {
             </Button>
           </div>
         </div>
-        <div className="hidden xl:flex">
-          <Image src={img} alt="foto-ze" width={540} />
+        <div className="hidden xl:flex  -mb-5">
+          <Image src={img} alt="foto-ze" width={500} />
         </div>
       </div>
     </section>
